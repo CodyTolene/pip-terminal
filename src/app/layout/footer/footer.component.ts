651e34727870b6e0ff02bdc48244d7243c171dc6
Cody Tolene <codyatolene@gmail.com>
2025-06-16 02:59:29 -0500

    Quick patch for header size and resetting scripts on home page visit.

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'pip-footer',
   templateUrl: './footer.component.html',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
   styleUrl: './footer.component.scss',
   providers: [],
   standalone: true,
