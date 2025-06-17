f0ee6323893fbc86529824c3532b07547871edad
Cody Tolene <codyatolene@gmail.com>
2025-06-17 12:33:29 -0500

    Use an iframe for displaying the official Mod Tool from The Wand Company

--- src/app/constants/pip-stylesheets.ts
+++
@@ -1,8 +0,0 @@
-export const PIP_STYLESHEETS = {
-  spectre: 'css/spectre.min.css',
-  'spectre-exp': 'css/spectre-exp.min.css',
-  'spectre-icons': 'css/spectre-icons.min.css',
-  main: 'css/main.css',
-} as const;
-
-export type StylesheetKey = keyof typeof PIP_STYLESHEETS;
