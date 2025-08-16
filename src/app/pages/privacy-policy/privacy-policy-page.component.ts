c7e330c62e542fa5c7e1193ace0cef38d3a7d83d
Cody Tolene <codyatolene@gmail.com>
2025-08-16 02:24:57 -0500

    Add privacy policy content.

--- src/app/pages/privacy-policy/privacy-policy-page.component.ts
+++ src/app/pages/privacy-policy/privacy-policy-page.component.ts
@@ -1,9 +1,11 @@
+import { PipFooterComponent } from 'src/app/layout';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'pip-privacy-policy-page',
   templateUrl: './privacy-policy-page.component.html',
-  imports: [],
+  imports: [PipFooterComponent],
   styleUrls: ['./privacy-policy-page.component.scss'],
   standalone: true,
 })
--- src/app/pages/status/status-page.component.ts
+++ src/app/pages/status/status-page.component.ts
@@ -1,9 +1,11 @@
+import { PipFooterComponent } from 'src/app/layout';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'pip-status-page',
   templateUrl: './status-page.component.html',
-  imports: [],
+  imports: [PipFooterComponent],
   styleUrls: ['./status-page.component.scss'],
   standalone: true,
 })
