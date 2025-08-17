78ec9df7f7a1e46a5717a61b3e61fb44fc6f19ef
Cody Tolene <codyatolene@gmail.com>
2025-08-17 14:08:48 -0500

    Add menu text with anim

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,7 +2,7 @@ import { PAGES } from 'src/app/routing';
 import { isNavbarOpenSignal } from 'src/app/signals';
 
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
@@ -18,7 +18,12 @@ export class HeaderComponent {
   protected readonly homeUrl = PAGES['Home'];
   protected readonly isNavbarOpenSignal = isNavbarOpenSignal;
 
+  @Input({ required: false })
+  public cursorSide: CursorSide = 'right';
+
   protected toggleNavbar(): void {
     this.isNavbarOpenSignal.set(!this.isNavbarOpenSignal());
   }
 }
+
+type CursorSide = 'left' | 'right';
