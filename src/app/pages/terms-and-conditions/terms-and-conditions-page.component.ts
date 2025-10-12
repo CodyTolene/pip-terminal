d4f860491a3aed4f6fe75e4c0b0d613752b59e36
Cody Tolene <codyatolene@gmail.com>
2025-10-12 02:37:53 -0500

    Bit of cleanup before EOD

--- src/app/pages/terms-and-conditions/terms-and-conditions-page.component.ts
+++ src/app/pages/terms-and-conditions/terms-and-conditions-page.component.ts
@@ -2,10 +2,12 @@ import { PipFooterComponent } from 'src/app/layout';
 
 import { Component } from '@angular/core';
 
+import { PipTitleComponent } from 'src/app/components/title/title.component';
+
 @Component({
   selector: 'pip-terms-and-conditions-page',
   templateUrl: './terms-and-conditions-page.component.html',
-  imports: [PipFooterComponent],
+  imports: [PipFooterComponent, PipTitleComponent],
   styleUrls: ['./terms-and-conditions-page.component.scss'],
   standalone: true,
 })
