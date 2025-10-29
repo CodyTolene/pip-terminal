6bc35fea7643e00738f6c235b3fac45e15d708ee
Cody Tolene <codyatolene@gmail.com>
2025-10-28 19:14:35 -0500

    Add firmware selector for file/dropdown.

--- src/app/components/companion/actions-firmware-upgrade/pip-actions-firmware-formgroup.ts
+++ src/app/components/companion/actions-firmware-upgrade/pip-actions-firmware-formgroup.ts
@@ -1,12 +1,14 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 export interface PipActionsFirmwareFormGroup {
+  customFirmware: FormControl<string | null>;
   firmwareDropdown: FormControl<string | null>;
   skipMediaFilesCheckbox: FormControl<boolean | null>;
 }
 
 export const pipActionsFirmwareFormGroup =
   new FormGroup<PipActionsFirmwareFormGroup>({
+    customFirmware: new FormControl<string | null>(null, [Validators.required]),
     firmwareDropdown: new FormControl<string | null>(null, [
       Validators.required,
     ]),
--- src/app/components/companion/actions-firmware-upgrade/pip-actions-firmware-upgrade.component.ts
+++ src/app/components/companion/actions-firmware-upgrade/pip-actions-firmware-upgrade.component.ts
@@ -1,21 +1,28 @@
+/* eslint-disable no-console */
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import {
   FormDirective,
   InputCheckboxComponent,
   InputDropdownComponent,
   InputDropdownOptionComponent,
 } from '@proangular/pro-form';
+import { isNonEmptyValue } from 'src/app/utilities';
 import { environment } from 'src/environments/environment';
 
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
+import { PipButtonComponent } from 'src/app/components/button/pip-button.component';
 import {
   PipActionsFirmwareFormGroup,
   pipActionsFirmwareFormGroup,
 } from 'src/app/components/companion/actions-firmware-upgrade/pip-actions-firmware-formgroup';
+import { PipFileUploadComponent } from 'src/app/components/file-upload/file-upload.component';
+import { PipTitleComponent } from 'src/app/components/title/title.component';
 
+@UntilDestroy()
 @Component({
   selector: 'pip-actions-firmware-upgrade',
   templateUrl: './pip-actions-firmware-upgrade.component.html',
@@ -28,16 +35,78 @@ import {
     InputDropdownComponent,
     InputDropdownOptionComponent,
     MatProgressBarModule,
+    PipButtonComponent,
+    PipFileUploadComponent,
+    PipTitleComponent,
     ReactiveFormsModule,
   ],
 })
-export class PipActionsFirmwareUpgradeComponent extends FormDirective<PipActionsFirmwareFormGroup> {
+export class PipActionsFirmwareUpgradeComponent
+  extends FormDirective<PipActionsFirmwareFormGroup>
+  implements OnInit
+{
   protected readonly releases = releases;
 
   protected override readonly formGroup = pipActionsFirmwareFormGroup;
+
+  protected readonly isUpgrading = signal(false);
+
+  public ngOnInit(): void {
+    this.formGroup.valueChanges.pipe(untilDestroyed(this)).subscribe(() => {
+      this.updateFormControlState();
+    });
+  }
+
+  public async onClearCustomFirmwareClick(): Promise<void> {
+    this.formGroup.controls.customFirmware.setValue(null);
+  }
+
+  public async onUpgradeClick(): Promise<void> {
+    if (this.formGroup.invalid) {
+      this.highlightInvalidControls();
+      this.scrollToFirstInvalidControl();
+      return;
+    }
+  }
+
+  private updateFormControlState(): void {
+    const useOfficialFirmware = isNonEmptyValue(
+      this.formGroup.controls.firmwareDropdown.value,
+    );
+
+    if (useOfficialFirmware) {
+      this.formGroup.controls.customFirmware.setValue(null, {
+        emitEvent: false,
+      });
+      this.formGroup.controls.customFirmware.disable({ emitEvent: false });
+      console.log('Using official firmware');
+      return;
+    } else {
+      this.formGroup.controls.customFirmware.enable({ emitEvent: false });
+    }
+
+    const userCustomFirmware = isNonEmptyValue(
+      this.formGroup.controls.customFirmware.value,
+    );
+    if (userCustomFirmware) {
+      this.formGroup.controls.firmwareDropdown.setValue(null, {
+        emitEvent: false,
+      });
+      this.formGroup.controls.firmwareDropdown.disable({ emitEvent: false });
+      console.log('Using unofficial firmware');
+      return;
+    } else {
+      this.formGroup.controls.firmwareDropdown.enable({ emitEvent: false });
+    }
+  }
 }
 
 const releases = [
+  {
+    label: '2v24.206-0.20',
+    // No longer available remotely.
+    url: 'firmware/release_2v24.206-0.20.zip',
+  },
   {
     label: '2v24.413-1.12',
     url: environment.isProduction
