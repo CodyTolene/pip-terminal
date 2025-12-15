8bcd9431cbfd9ee80bad2e51e21f70df21afc7ef
Cody Tolene <codyatolene@gmail.com>
2025-12-15 16:01:42 -0600

    Update cypress test

--- cypress/e2e/welcome.cy.ts
+++ cypress/e2e/welcome.cy.ts
@@ -84,7 +84,7 @@ describe('Welcome page', () => {
         .scrollIntoView()
         .should('be.visible');
       cy.get('div[aria-label="Developers"]').within(() => {
-        cy.get('a.pip-grid-item').should('have.length', 10);
+        cy.get('a.pip-grid-item').should('have.length', 11);
         cy.contains('h4', 'gfwilliams').scrollIntoView().should('be.visible');
       });
     });
