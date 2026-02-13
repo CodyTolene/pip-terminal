cc85825943a4b10fc402a4767e02f76f4e42491d
Cody Tolene <CodyTolene@users.noreply.github.com>
2026-02-13 17:40:07 -0600

    v3.30.0 - Package updates (#244) (#245)
    * Version uptick
    
    * ng update @angular/cli
    
    * ng update @angular/core
    
    * Remove Google Maps
    
    * Package lock updates
    
    * ng update @angular/material
    
    * Node engine updates
    
    * Cycle API key
    
    * Package updates
    
    * ng update angular-eslint
    
    * API package updates
    
    * Package-lock updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,16 +6,13 @@ export const environment: EnvironmentCredentials = {
   appsUrl: 'https://raw.githubusercontent.com/CodyTolene/pip-apps/releases',
   google: {
     firebase: {
-      apiKey: 'AIzaSyD0ESBvQ6Ok20ddJmS0kI7K-N6u6LXXlyw',
+      apiKey: 'AIzaSyDDQ5YRhwcHP8m46TRe9XagXwHSeXdbSBE',
       appId: '1:438882577130:web:dc1124a2ab2ad5d21c0593',
       authDomain: 'pip-boy.local', // 'pip-terminal.firebaseapp.com',
       measurementId: 'G-1Z6DNZH2C2',
       messagingSenderId: '438882577130',
       projectId: 'pip-terminal',
       storageBucket: 'pip-terminal.firebasestorage.app',
-    },
-    maps: {
-      apiKey: undefined,
     },
     recaptcha: {
       apiKey: undefined,
